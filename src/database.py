d6413f1ccd4bb90c948e92a534090587a6ac2a20
vinniedotsys <vincent.hassen@laposte.net>
2026-06-11 22:53:24 +0200

    all DBObjects

--- src/database.py
+++ src/database.py
@@ -2,6 +2,8 @@ import os
 import sqlite3
 import uuid
 
+from typing import Optional
+
 class DBObject:
     TABLE = ""
     FIELDS = ""
@@ -31,13 +33,41 @@ class DBObject:
 class Players(DBObject):
     TABLE = "players"
     FIELDS = "(id TEXT PRIMARY KEY, name TEXT)"
-    def __init__(self, db_path, name, id=uuid.uuid4()) -> None:
+    def __init__(self, db_path, id=uuid.uuid4()) -> None:
         super().__init__(db_path, id)
-        self.name = name
+        self.name: Optional[str] = None
 
 class Games(DBObject):
     TABLE = "games"
-    FIELDS = "(id TEXT PRIMARY KEY, question_number INTEGER, winner TEXT, gamemaster TEXT, FOREIGN KEY(winner,gamemaster) REFERENCES player(id,id))"
-    def __init__(self, db_path, question_number=0, id=uuid.uuid4()) -> None:
+    FIELDS = "(id TEXT PRIMARY KEY, question_number INTEGER, winner TEXT, gamemaster TEXT, FOREIGN KEY(winner,gamemaster) REFERENCES players(id,id))"
+    def __init__(self, db_path, id=uuid.uuid4()) -> None:
         super().__init__(db_path, id)
-        self.question_number = question_number
+        self.question_number: Optional[int] = None
+
+class Questions(DBObject):
+    TABLE = "questions"
+    FIELDS = "(id TEXT PRIMARY KEY, game TEXT, answer TEXT, number INTEGER, FOREIGN KEY(game) REFERENCES games(id), FOREIGN KEY(answer) REFERENCES choices(id))"
+    def __init__(self, db_path, id=uuid.uuid4()) -> None:
+        super().__init__(db_path, id)
+        self.game: Optional[str] = None
+        self.answer: Optional[str] = None
+        self.order: Optional[int] = None
+
+class Choices(DBObject):
+    TABLE = "choices"
+    FIELDS = "(id TEXT PRIMARY KEY, emoji TEXT, question TEXT, FOREIGN KEY(question) REFERENCES games(id))"
+    def __init__(self, db_path, id=uuid.uuid4()) -> None:
+        super().__init__(db_path, id)
+        self.emoji: Optional[str] = None
+        self.question: Optional[str] = None
+
+class PlayerAnswers(DBObject):
+    TABLE = "player_answers"
+    FIELDS = "(id TEXT PRIMARY KEY, question TEXT, player TEXT, answer TEXT, result INTEGER, FOREIGN KEY(question) REFERENCES questions(id), FOREIGN KEY(player) REFERENCES players(id), FOREIGN KEY(answer) REFERENCES choices(id))"
+    def __init__(self, db_path, id=uuid.uuid4()) -> None:
+        super().__init__(db_path, id)
+        self.question: Optional[str] = None
+        self.player: Optional[str] = None
+        self.anwser: Optional[str] = None
+        self.result: Optional[int] = None
+
--- tests/test_dbojects.py
+++ tests/test_dbojects.py
@@ -6,7 +6,7 @@ from src.database import *
 
 class DBOTest(unittest.TestCase):
     def test_empty_db(self):
-        test_player = Players('data/test.db', 'LouisdeGie')
+        test_player = Players('data/test.db')
         test_path = os.path.isfile('data/test.db')
         self.assertEqual(test_path, True)
 
@@ -24,7 +24,16 @@ class DBOTest(unittest.TestCase):
         cur = con.cursor()
         res = cur.execute("PRAGMA table_info('games')")
         tables = res.fetchall()
-        print(tables)
         test_table = "games" in tables
         self.assertEqual(True, True)
+
+    def test_remaing_objects(self):
+        test_choice = Choices('data/test.db')
+        test_question = Questions('data/test.db')
+        test_player_answer = PlayerAnswers('data/test.db')
+        con = sqlite3.connect('data/test.db')
+        cur = con.cursor()
+        res = cur.execute("SELECT name FROM sqlite_master")
+        tables = res.fetchall()
+        print(tables)
 
