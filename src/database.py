4fc2d19aa37be6dc88a84f44e132669dbab11d48
vinniedotsys <vincent.hassen@laposte.net>
2026-06-16 22:58:52 +0200

    starting get method

--- src/database.py
+++ src/database.py
@@ -8,8 +8,10 @@ class DBObject:
     TABLE = ""
     FIELDS = ""
 
-    def __init__(self, db_path, id=str(uuid.uuid4())) -> None:
+    def __init__(self, db_path, id=str(uuid.uuid7().hex)) -> None:
         self.db_path = db_path
+        if len(id) != 32:
+            raise ValueError("Not a valid ID")
         self.id = id
         self.check_db()
         self.check_table()
@@ -30,6 +32,16 @@ class DBObject:
             cur.execute(f"CREATE TABLE {self.TABLE}{self.FIELDS}")
         con.close()
 
+    def get(self, id):
+        if len(id) != 32:
+            raise ValueError("Not a valid ID")
+        con = sqlite3.connect(self.db_path)
+        cur = con.cursor()
+        res = cur.execute(f"SELECT * FROM {self.TABLE} WHERE id='?'", id)
+        if res.fetchone() is None:
+            raise Exception("Record not found")
+
+
     def insert(self, *fields):
         diff = len(vars(self)) - len(fields)
         diff -= 2 #always self defined
@@ -45,21 +57,21 @@ class DBObject:
 class Players(DBObject):
     TABLE = "players"
     FIELDS = "(id TEXT PRIMARY KEY, name TEXT)"
-    def __init__(self, db_path, id=str(uuid.uuid4())) -> None:
+    def __init__(self, db_path, id=str(uuid.uuid7().hex)) -> None:
         super().__init__(db_path, id)
         self.name: Optional[str] = None
 
 class Games(DBObject):
     TABLE = "games"
     FIELDS = "(id TEXT PRIMARY KEY, question_number INTEGER, winner TEXT, gamemaster TEXT, FOREIGN KEY(winner,gamemaster) REFERENCES players(id,id))"
-    def __init__(self, db_path, id=str(uuid.uuid4())) -> None:
+    def __init__(self, db_path, id=str(uuid.uuid7().hex)) -> None:
         super().__init__(db_path, id)
         self.question_number: Optional[int] = None
 
 class Questions(DBObject):
     TABLE = "questions"
     FIELDS = "(id TEXT PRIMARY KEY, game TEXT, answer TEXT, number INTEGER, FOREIGN KEY(game) REFERENCES games(id), FOREIGN KEY(answer) REFERENCES choices(id))"
-    def __init__(self, db_path, id=str(uuid.uuid4())) -> None:
+    def __init__(self, db_path, id=str(uuid.uuid7().hex)) -> None:
         super().__init__(db_path, id)
         self.game: Optional[str] = None
         self.answer: Optional[str] = None
@@ -68,7 +80,7 @@ class Questions(DBObject):
 class Choices(DBObject):
     TABLE = "choices"
     FIELDS = "(id TEXT PRIMARY KEY, emoji TEXT, question TEXT, FOREIGN KEY(question) REFERENCES games(id))"
-    def __init__(self, db_path, id=str(uuid.uuid4())) -> None:
+    def __init__(self, db_path, id=str(uuid.uuid7().hex)) -> None:
         super().__init__(db_path, id)
         self.emoji: Optional[str] = None
         self.question: Optional[str] = None
@@ -76,7 +88,7 @@ class Choices(DBObject):
 class PlayerAnswers(DBObject):
     TABLE = "player_answers"
     FIELDS = "(id TEXT PRIMARY KEY, question TEXT, player TEXT, answer TEXT, result INTEGER, FOREIGN KEY(question) REFERENCES questions(id), FOREIGN KEY(player) REFERENCES players(id), FOREIGN KEY(answer) REFERENCES choices(id))"
-    def __init__(self, db_path, id=str(uuid.uuid4())) -> None:
+    def __init__(self, db_path, id=str(uuid.uuid7().hex)) -> None:
         super().__init__(db_path, id)
         self.question: Optional[str] = None
         self.player: Optional[str] = None
