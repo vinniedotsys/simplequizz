e798dccd50cdcffb05f2b0f33dfe38776fc8695b
vinniedotsys <vincent.hassen@laposte.net>
2026-07-13 17:53:36 +0200

    create quiz research

--- src/create_quiz.py
+++ src/create_quiz.py
@@ -1,3 +1,9 @@
 import os
+import json
+import base64
 
-from src.database import * 
+from src.database import *
+
+def image_to_base64(path):
+    if not os.path.isfile(path):
+        raise Exception(f"Image not found : {path}")
