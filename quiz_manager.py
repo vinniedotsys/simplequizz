034c1f6793d02a81cc1224793fd1a4655701ee59
vinniedotsys <vincent.hassen@laposte.net>
2026-09-02 22:06:34 +0200

    implementing script

---
+++ quiz_manager.py
@@ -0,0 +1,5 @@
+import sys
+
+from src.quiz_admin import check_and_exec
+
+check_and_exec(sys.argv[1], sys.argv[2:])
--- src/quiz_admin.py
+++ src/quiz_admin.py
@@ -4,13 +4,14 @@ import sys
 from src.database import *
 
 
-def check_and_exec(function, *params):
+def check_and_exec(function, params):
     current_module = sys.modules[__name__]
     functions_list = [name for name, obj in inspect.getmembers(current_module, inspect.isfunction)]
     
     if function in functions_list:
         func = getattr(current_module, function)
-        func(params)
+        print(*params)
+        func(*params)
     else:
         print("Valid functions are : ", functions_list)
 
